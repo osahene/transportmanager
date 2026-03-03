3139e475f590d49b23d8b5219ebf8532de0ca48c
osahene <bra.kwameadu3@gmail.com>
2026-03-03 23:39:45 +0000

    fixing errors

--- app/sw.ts
+++ app/sw.ts
@@ -16,16 +16,6 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
-  fallbacks: {
-    entries: [
-      {
-        url: "/~offline",
-        matcher({ request }) {
-          return request.destination === "document";
-        },
-      },
-    ],
-  },
 });
 
 serwist.addEventListeners();
